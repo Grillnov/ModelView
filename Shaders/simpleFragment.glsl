19d2439937bf43b08c651537c28797e9f2fcdf28
AIPC <2540198460@qq.com>
2016-09-24 23:09:03 +0800

    removal of files.

--- Shaders/simpleFragment.glsl
+++
@@ -1,9 +0,0 @@
-# version 430 core
-
-smooth in vec4 vFragColor;
-out vec4 pixColor;
-
-void main()
-{
-	pixColor = vFragColor;
-}
--- src/TextureCube.cpp
+++
@@ -1,7 +0,0 @@
-//
-//  TextureCube.cpp
-//  ModelView
-//
-//  Created by Bowen Yang on Sept 18, 2016.
-//  Copyright (c) 2016 Bowen Yang. All rights reserved.
-//
